1caded20774b9b40ef054c3271292437a47f3f64
matejkralj <matejkralj.grosuplje@gmail.com>
2024-04-21 03:54:29 +0200

    verson 1.1

--- website/main.py
+++ website/main.py
@@ -1,5 +1,20 @@
 from flask import Flask, render_template, request, redirect
 
+<<<<<<< Updated upstream
+=======
+def get_brands_and_models():
+    brands=[]
+    models=[]
+    with open('static/Tables/znamke_modeli.csv', mode='r') as infile:
+        reader = csv.reader(infile)
+        for row in reader:
+            brands.append(row[0])
+            models.append(row[1])
+    models = models[:]
+    brands=list(set(brands))
+    return brands, models
+brands, models = get_brands_and_models()
+>>>>>>> Stashed changes
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,6 +30,12 @@ def about():
         kilometers_range_end = request.form['kilometers_range_end']
         price_range_start = request.form['price_range_start']
         price_range_end = request.form['price_range_end']
+<<<<<<< Updated upstream
+=======
+        brand = request.form.get('brand')
+        fuel_type = request.form.get('fuel_type')
+        frequency = request.form.get('frequency')
+>>>>>>> Stashed changes
         if kilometers_range_start == "":
             kilometers_range_start = 0
         if kilometers_range_end == "":
@@ -29,6 +50,12 @@ def about():
         print(kilometers_range_end)
         print(price_range_start)
         print(price_range_end)
+<<<<<<< Updated upstream
+=======
+        print(brand)
+        print(fuel_type)
+        print(frequency)
+>>>>>>> Stashed changes
         return redirect('avtonet')
     
     return render_template('avtonet.html')
@@ -43,7 +70,11 @@ def avtonet():
         return redirect(url_for('index'))
 
     # show the form, it wasn't submitted
+<<<<<<< Updated upstream
     return render_template('avtonet.html', carbrands=["Audi", "Toyota", "Bmw", "Volkwagen"])
+=======
+    return render_template('avtonet.html', carbrands=brands, models=models, fuel_types=["","Bencin", "Dizel", "Elektrika", "Hibrid", "Plin"] , frequency=["Immediate", "Daily", "Weekly", "Monthly", "Yearly"])
+>>>>>>> Stashed changes
 @app.route('/nepremicnine', methods=['GET', 'POST'])
 def nepremicnine():
     if request.method == 'POST':
